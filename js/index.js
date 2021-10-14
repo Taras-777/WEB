f80b3a5a88826405c037f97bb37dac417762a7cc
Taras-777 <74727526+Taras-777@users.noreply.github.com>
2021-10-14 12:48:42 +0300

    code lab4

--- js/index.js
+++ js/index.js
@@ -1,5 +1,13 @@
-import { renderItemsList } from "./dom-util.js"
-import { getAllPlane } from "./api.js";
+import {
+  getInputValues,
+  renderItemsList,
+  EDIT_BUTTON_PREFIX,
+  clearInputs
+} from "./dom-util.js"
+import { getAllPlanes, postPlane, updatePlane } from "./api.js";
+
+const formFields = document.getElementsByClassName("form-control");
+const submitButton = document.getElementById("submit_button");
 
 const searchButton = document.getElementById("search_button");
 const clearSearchButton = document.getElementById("clear_search_button");
@@ -7,46 +15,86 @@ const searchInput = document.getElementById("search_input");
 const sortCheckbox = document.getElementById("sort_checkbox");
 const countButton = document.getElementById("count_button");
 
-let plane = [];
+let planes = [];
 
-export const refetchAllPlane = () => {
-  const allPlane = getAllPlane();
+const onEditItem = (e) => {
+  if (!validateInput()) {
+    return;
+  };
+  const itemId = e.target.id.replace(EDIT_BUTTON_PREFIX, "");
 
-  plane = allPlane.sort((a, b) => b.title.localeCompare(a.title));
+  updatePlane(itemId, getInputValues());
+  clearInputs();
 
-  renderItemsList(plane);
+  refetchAllPlanes();
 };
 
-searchButton.addEventListener("click", () => {
-  const foundPlane = plane.filter(
-    (plane) => plane.title.search(searchInput.value) !== -1);
+export const refetchAllPlanes = () => {
+  const allPlanes = getAllPlanes();
 
-  renderItemsList(foundPlane);
+  planes = allPlanes.sort((a,b) =>b.model.localeCompare(a.model));
+
+  renderItemsList(planes, onEditItem);
+};
+
+const validateInput = () => {
+  if (Array.from(formFields).filter(x => x.value.trim() == "").fuel != 0) {
+    alert("Please fill out required fields"); 
+    return false;
+  }
+}
+
+submitButton.addEventListener("click", (event) => {
+  if (!validateInput()) {
+    return;
+  };
+
+    event.preventDefault();
+
+    const { model, description, fuel, passengers } = getInputValues();
+
+    clearInputs();
+
+    postPlane({
+      model,
+      description,
+      fuel,
+      passengers
+    });
+  
+    refetchAllPlanes();
+});
+
+searchButton.addEventListener("click", () => {
+  const foundPlanes = planes.filter(
+    (plane) => plane.model.search(searchInput.value) !== -1);
+    
+  renderItemsList(foundPlanes, onEditItem);
 });
 
 clearSearchButton.addEventListener("click", () => {
-  renderItemsList(plane);
+  renderItemsList(planes, onEditItem);
 
   searchInput.value = "";
 });
 
 sortCheckbox.addEventListener("change", function (e) {
-  if (this.checked) {
-    const sortedPlane = palne.sort(
-      (a, b) => parseFloat(a.passengers) - parseFloat(b.passengers));
+    if (this.checked) {
+        const sortedPlanes = planes.sort(
+            (a, b) => parseFloat(a.passengers) - parseFloat(b.passengers));
 
-    renderItemsList(sortedPlane);
-  }
-  else {
-    refetchAllPlane();
-  }
+        renderItemsList(sortedPlanes, onEditItem);
+    }
+    else {
+        refetchAllPlanes();
+    }
 });
 
 countButton.addEventListener("click", () => {
-  let sum = plane.map(o => o.fuel).reduce((a, c) => { return a + c });
-  document.getElementById("total-fuel").innerText = sum;
-  console.log(sum);
+    let sum = planes.map(o => o.fuel).reduce((a, c) => { return a + c });
+    document.getElementById("total-time").innerText = sum;
+    console.log(sum);
 })
 
 // main code
-refetchAllPlane();
+refetchAllPlanes();
